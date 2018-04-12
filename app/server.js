25956e7d0b54c2d9b0aeaac4240a4fb09bae9719
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-04-11 22:23:27 -0300

    removes temporary user api mock stuff

--- app/server.js
+++ app/server.js
@@ -2,10 +2,8 @@ require('dotenv').load();
 
 var server = {};
 var express = require('express');
-var bodyParser = require("body-parser");
 var cors = require('cors');
 var expressApp = express();
-var FlickrService = require('./flickr-service');
 
 server.request = require('request');
 server.app = require('http').createServer(expressApp);
@@ -18,8 +16,6 @@ var corsOptions = {
 }
 
 expressApp.use(cors(corsOptions));
-expressApp.use(bodyParser.urlencoded({limit: '5mb', extended: false}));
-expressApp.use(bodyParser.json({limit: '5mb'}));
 
 server.light = 0;
 server.touch = 0;
@@ -57,37 +53,6 @@ expressApp.get('/set-light/:value', function(req, res) {
 
   var ip = getIp(req);
   logExceptOnTest("set-light/" + server.light + " from: " + ip);
-});
-
-expressApp.get('/person/:code/results', function(req, res) {
-  var userCode = parseInt(req.params.code || 0);
-  if(userCode == '123456') {
-    var user = {
-      email: 'foo@bar.com',
-      name: 'John Wayne',
-      profile: 2
-    };
-
-    server.sendTextResponse(res, 200, JSON.stringify(user));
-  } else {
-    server.sendTextResponse(res, 404, "User Not Found");
-  }
-
-  var ip = getIp(req);
-  logExceptOnTest("user/" + userCode + " from: " + ip);
-});
-
-expressApp.post('/person/:code/photo', function(req, res) {
-  req.body.code = req.params.code;
-
-  var flickerService = new FlickrService(req.body);
-  flickerService.updatePersonData(function(err, data) {
-    if(err) {
-      server.sendTextResponse(res, 500, JSON.stringify(err));
-    } else {
-      server.sendTextResponse(res, 200, JSON.stringify(data));
-    }
-  });
 });
 
 expressApp.get('*', function (req, res) {
